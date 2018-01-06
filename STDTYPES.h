8d5397da9509bcd5160c63135bce73a225d050ee
Asmaa Mohamed Mohamed <33968945+AsmaaMohamedMohamed@users.noreply.github.com>
2018-01-06 02:02:34 +0200

    Add files via upload

---
+++ STDTYPES.h
@@ -0,0 +1,25 @@
+/*
+ * STDTYPES.h
+ * standard data types definitions
+ *
+ *  Created on:30/12/2017
+ *      Author: Asmaa
+ */
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+typedef unsigned char u8;
+typedef unsigned short int u16;
+typedef unsigned long int u32;
+
+typedef signed char s8;
+typedef signed short int s16;
+typedef signed long int s32;
+
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+
+
+#endif /* STDTYPES_H_ */
